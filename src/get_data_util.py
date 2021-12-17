63aefd0984a611369c7c2a1e6a1112ac00166275
Vikram Nayyar <vikramnayyar@live.com>
2021-12-17 11:15:15 +0530

    Removed redundant comments

--- src/get_data_util.py
+++ src/get_data_util.py
@@ -8,14 +8,6 @@ import pandas as pd
 from logzero import logger, logfile
 
 
-# def download_dataset():
-#     file_id = config["get_data"]["download_id"]   # read dataset download id
-#     destination = pathlib.Path.cwd().parent.joinpath('data/restnt_data.csv')
-#     download_file_from_google_drive(file_id, destination)
-#     logger.info('Dataset was successfully downloaded')
-
-
-
 def analyze_data(df): 
     logger.info('\n * Size of dataframe: {}\n'.format(df.shape))
     logger.info('* Datatype of columns are:')
@@ -25,12 +17,9 @@ def analyze_data(df):
     logger.info('Total NaNs:{}'.format(df.isnull().sum().sum()))
 
 
-
 def extract_dict(df, col):
     feature = df[col].astype('category')
     dict_val = dict(enumerate(feature.cat.categories))
     dict_inv = {a:b for b,a in dict_val.items()}
     
     return dict_inv
-
-
