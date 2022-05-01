089da35c886b4e2f387be5cfdd12755009d4a2fa
Vikram Nayyar <vikramnayyar@live.com>
2022-05-01 13:29:34 +0530

    minor correction

--- src/run_project.py
+++ src/run_project.py
@@ -5,7 +5,7 @@ The script sequentially runs all the project scripts.
 
 import subprocess
 
-program_list = ['get_data.py', 'data_analysis.py', 'prepare_data.py', 'split_data.py', 'model_data.py']
+program_list = ['src/get_data.py', 'src/data_analysis.py', 'src/prepare_data.py', 'src/split_data.py', 'src/model_data.py']
 
 for program in program_list:
     subprocess.call(['python3', program])
